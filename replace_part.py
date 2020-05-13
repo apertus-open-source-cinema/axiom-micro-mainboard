640c817bc9659501287fc17ea25054f218961a36
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2020-05-13 17:33:13 +0200

    make caps the proper part number

--- replace_part.py
+++ replace_part.py
@@ -13,9 +13,19 @@ from horizon_util import load_schematic, save_schematic, load_from_cache
 # from_id = ["41a1650b-0dd1-4528-b38e-e23a3d7f2803"]
 
 # 10 nF 0402 cpl cap / tdk cap
-from_id = ["c5d6df18-b91b-4bac-b859-1921abfe57ed", "f3a3f65f-1d3d-4c47-974d-eab2fa8bc6bd"]
+# from_id = ["c5d6df18-b91b-4bac-b859-1921abfe57ed", "f3a3f65f-1d3d-4c47-974d-eab2fa8bc6bd"]
 
+# 1nF 0402 cpl
+# from_id = ["16d54586-4bbf-4959-a195-6a69989eb0fa"]
 
+# 47pF 0402 cpl
+# from_id = ["2cd1846f-151f-4176-8068-e0d7e01140a4"]
+
+# 220nF 0402 tdk
+# from_id = ["25bca914-4df8-415c-8614-ecec0326b359"]
+
+# 2.2uF 0603 tdk
+from_id = ["f6e8e98a-a1ea-4ec5-98ca-1e9f3fd34826"]
 
 
 # 22 uF 0603 jlcpcb basic part
@@ -31,9 +41,24 @@ from_id = ["c5d6df18-b91b-4bac-b859-1921abfe57ed", "f3a3f65f-1d3d-4c47-974d-eab2
 # to_id = "2f8b1096-3213-4ec5-8dfd-499f2136c929"
 
 # 10nF 0402 jlcpcb basic part
-to_id = "168c28aa-ebee-4ea6-92a5-b39708200d01"
+# to_id = "168c28aa-ebee-4ea6-92a5-b39708200d01"
 
-dump_parts = True
+# 1nF 0402 jlcpcb basic part
+# to_id = "358d010d-72df-4b22-9b16-a7eab1672a04"
+
+# 47nF 0603 jlcpcb basic part
+# to_id = "db995261-3300-407b-918c-8219976ae91c"
+
+# 47pF 0402 jlcpcb basic part
+# to_id = "2b2b0043-af6e-4af0-b1d4-bd9b3e3e7175"
+
+# 220nF 0402 jlcpcb basic part
+# to_id = "c2c1a5c8-6058-49b2-87ca-cc22c59a4ce9"
+
+# 2.2uF 0603 jlcpcb basic part
+to_id = "147da013-b612-45cc-aa1c-c2a8b78a6765"
+
+dump_parts = False
 
 if dump_parts:
     parts = load_from_cache("part")
